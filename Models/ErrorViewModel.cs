c03afab28ed31d3c9b7ccaa5d857a64350bc15f0
Blake Bordovsky <blakebordovsky@gmail.com>
2019-10-02 19:49:11 -0500

    - Add initial files for .NET Core 3.0 MVC Web Application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCorePortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
